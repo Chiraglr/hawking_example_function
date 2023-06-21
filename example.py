f15817d28ad7d50a53401c0f55fb73d2357bcce2
Chirag Ramachandraiah <chirag032@yahoo.com>
2023-06-22 03:18:26 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,5 @@
 from datetime import datetime, timezone
 ​
-​
-# def get_request(query, api_key, api_url):
-#     headers = {"Authorization": api_key}
-#     response = requests.post(url=api_url, json={'query': query}, headers=headers)
-#     if response.status_code == 200:
-#         response = response.json()
-#         return response["data"]
-#     else:
-#         return {}
-​
-​
 def build_response(data, limit, cursor_value):
     response = dict()
 ​
@@ -28,14 +17,6 @@ def build_response(data, limit, cursor_value):
 ​
 ​
 def lambda_handler(request, context):
-    # Import credentials
-    # These and other parameters should be wrapped up in 'request,' which is relayed from the connector's 'secrets'
-    # api_key = request['secrets']['apiKey']
-​
-    # Get data
-    # api_url = "https://api.monday.com/v2"
-    # boards_query = '{ boards {id name state board_folder_id } }'
-    # boards_data = get_request(boards_query, api_key=api_key, api_url=api_url)
     boards_data = {
         "boards": [{
             "id": 1,
@@ -45,20 +26,12 @@ def lambda_handler(request, context):
             "value": "board 2"
         }]
     };
-​
-    # Get json from request
-    # request = requests.get(url=api_url, auth=api_key).json()
-    # Note that get_json above is a flask function, which seems to work because google cloud functions are run
-    # in a flask based environment, even though we didn't import it explicitly or add it to requirements.txt
-​
-    # Set state
+
     try:
         cursor_value = request['state']['cursor']
     except KeyError:
         cursor_value = '1970-01-01T00:00:00'
 ​
-    # Set the 'limit' according to your estimates of the table's size and row count
-    # Again, these can also be stored in 'request'
     limit = int(request['secrets']['limit'])
 ​
     if len(boards_data) == 0:
